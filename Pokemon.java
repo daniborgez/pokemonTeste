19bf2583246588193572a2945c3aefc07861c965
Daniel Borges <daniel0107borges@gmail.com>
2024-05-08 22:58:14 -0300

    primeiro commit

---
+++ Pokemon.java
@@ -0,0 +1,12 @@
+public class Pokemon {
+
+    public String nome; 
+    public String tipo; 
+
+
+public Pokemon(String nome, String tipo){
+    this.nome = nome;
+    this.tipo = tipo;
+}
+
+}
---
+++ TestePokemon.java
@@ -0,0 +1,9 @@
+public class TestePokemon {
+     public static void main(String[] args) {
+
+        Pokemon pokemon1 = new Pokemon("Pikachu", "aquatico");
+        System.out.println(pokemon1.nome + ", " + pokemon1.tipo);
+    
+}
+    
+}
