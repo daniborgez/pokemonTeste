8fb2c1aeaecdab5b6271cee3dfeca1450b37a7b5
Daniel Borges <daniel0107borges@gmail.com>
2024-05-08 23:26:54 -0300

    teste de id

--- Pokemon.java
+++ Pokemon.java
@@ -1,12 +1,14 @@
 public class Pokemon {
 
     public String nome; 
-    public String tipo; 
+    public String tipo;
+    public int id; 
 
 
 public Pokemon(String nome, String tipo){
     this.nome = nome;
     this.tipo = tipo;
+    this.id = id;
 }
 
 }
