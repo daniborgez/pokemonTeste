e080ac9670daf06a2a5d30bc32b9f7860087cb43
Daniel Borges <daniel0107borges@gmail.com>
2024-05-08 23:29:36 -0300

    testeCommitId

--- TestePokemon.java
+++ TestePokemon.java
@@ -1,6 +1,27 @@
 public class TestePokemon {
      public static void main(String[] args) {
 
+        //Teste de ID 
+        //public class User {  
+            // ...  
+          //  int id; // Receberá o valor de contadorDeId
+            // ...
+       // }
+        
+       // public class Mein {  
+           // public static void main(String[] args) {
+            //    int contadorDeId = 1;
+            //    User u1 = new User();
+            //    u1.id = contadorDeId++; // Atribui e depois incrementa contadorDeId para 2
+        
+                // ...  
+        
+             //   User u2 = new User();
+              //  u2.id = contadorDeId++; // Atribui e depois incrementa contadorDeId para 3...
+         //   }  
+        //}
+    
+
         Pokemon pokemon1 = new Pokemon("Pikachu", "aquatico");
         System.out.println(pokemon1.nome + ", " + pokemon1.tipo);
     
